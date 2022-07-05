fc7347412384eb7717a6bb63866f2ed21687ec88
DaCreeperGuy <Emily@cyberdude.com>
2022-07-05 19:37:39 +0100

    Add project files.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace RoHaxxBlocks
 {
-    partial class Form1
+    partial class RoHaxxBlocks
     {
         /// <summary>
         /// Required designer variable.
@@ -29,20 +29,201 @@ namespace RoHaxxBlocks
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoHaxxBlocks));
+            this.TopBar = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Execute = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.SaveFile = new System.Windows.Forms.Button();
+            this.Inject = new System.Windows.Forms.Button();
+            this.ScriptHub = new System.Windows.Forms.Button();
+            this.Settings = new System.Windows.Forms.Button();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.TopBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // TopBar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.TopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.TopBar.Controls.Add(this.label1);
+            this.TopBar.Location = new System.Drawing.Point(0, 0);
+            this.TopBar.Name = "TopBar";
+            this.TopBar.Size = new System.Drawing.Size(1000, 75);
+            this.TopBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 28F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RoHaxxBlocks - Version Dev";
+            // 
+            // Execute
+            // 
+            this.Execute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.Execute.ForeColor = System.Drawing.SystemColors.Control;
+            this.Execute.Location = new System.Drawing.Point(12, 438);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(150, 50);
+            this.Execute.TabIndex = 1;
+            this.Execute.Text = "Execute";
+            this.Execute.UseVisualStyleBackColor = false;
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.Clear.ForeColor = System.Drawing.SystemColors.Control;
+            this.Clear.Location = new System.Drawing.Point(168, 438);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(150, 50);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.OpenFile.ForeColor = System.Drawing.SystemColors.Control;
+            this.OpenFile.Location = new System.Drawing.Point(324, 438);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(150, 50);
+            this.OpenFile.TabIndex = 3;
+            this.OpenFile.Text = "Open File";
+            this.OpenFile.UseVisualStyleBackColor = false;
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.SaveFile.ForeColor = System.Drawing.SystemColors.Control;
+            this.SaveFile.Location = new System.Drawing.Point(480, 438);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(150, 50);
+            this.SaveFile.TabIndex = 4;
+            this.SaveFile.Text = "Save File";
+            this.SaveFile.UseVisualStyleBackColor = false;
+            // 
+            // Inject
+            // 
+            this.Inject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.Inject.ForeColor = System.Drawing.SystemColors.Control;
+            this.Inject.Location = new System.Drawing.Point(636, 438);
+            this.Inject.Name = "Inject";
+            this.Inject.Size = new System.Drawing.Size(150, 50);
+            this.Inject.TabIndex = 5;
+            this.Inject.Text = "Inject";
+            this.Inject.UseVisualStyleBackColor = false;
+            // 
+            // ScriptHub
+            // 
+            this.ScriptHub.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ScriptHub.ForeColor = System.Drawing.SystemColors.Control;
+            this.ScriptHub.Location = new System.Drawing.Point(792, 438);
+            this.ScriptHub.Name = "ScriptHub";
+            this.ScriptHub.Size = new System.Drawing.Size(150, 50);
+            this.ScriptHub.TabIndex = 6;
+            this.ScriptHub.Text = "Script Hub";
+            this.ScriptHub.UseVisualStyleBackColor = false;
+            // 
+            // Settings
+            // 
+            this.Settings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.Settings.ForeColor = System.Drawing.SystemColors.Control;
+            this.Settings.Location = new System.Drawing.Point(948, 438);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(40, 50);
+            this.Settings.TabIndex = 7;
+            this.Settings.Text = "⚙️";
+            this.Settings.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Settings.UseVisualStyleBackColor = false;
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>.+)\r\n";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(299, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.fastColoredTextBox1.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.CommentPrefix = "--";
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.ForeColor = System.Drawing.Color.White;
+            this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.Lua;
+            this.fastColoredTextBox1.LeftBracket = '(';
+            this.fastColoredTextBox1.LeftBracket2 = '{';
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(12, 82);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.RightBracket = ')';
+            this.fastColoredTextBox1.RightBracket2 = '}';
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(774, 350);
+            this.fastColoredTextBox1.TabIndex = 8;
+            this.fastColoredTextBox1.Text = "-- Made by @DaCreeperGuy on github";
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // RoHaxxBlocks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.ScriptHub);
+            this.Controls.Add(this.Inject);
+            this.Controls.Add(this.SaveFile);
+            this.Controls.Add(this.OpenFile);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.TopBar);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "RoHaxxBlocks";
+            this.Text = "RoHaxxBlocks";
+            this.TopBar.ResumeLayout(false);
+            this.TopBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel TopBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.Button SaveFile;
+        private System.Windows.Forms.Button Inject;
+        private System.Windows.Forms.Button ScriptHub;
+        private System.Windows.Forms.Button Settings;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RoHaxxBlocks
 {
-    public partial class Form1 : Form
+    public partial class RoHaxxBlocks : Form
     {
-        public Form1()
+        public RoHaxxBlocks()
         {
             InitializeComponent();
         }
